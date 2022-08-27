182f6a0e6cdebf7186de7ac9cffa8f55f8f4e138
RubenPari <rubenpari.developer@gmail.com>
2022-08-27 22:19:17 +0200

    defided character model

--- heros/models.py
+++ heros/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Characters(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    # array of urls
+    urls = models.TextField()
+
+    # image
+    thumbnail = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
