67a1899d0c28c9deaa3f77cc5aac1c1212c7def1
RubenPari <rubenpari.developer@gmail.com>
2022-08-31 11:10:19 +0200

    implemented CORS origins

--- manage_heros/settings.py
+++ manage_heros/settings.py
@@ -20,6 +20,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'heros.apps.HerosConfig',
+    'corsheaders',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 MIDDLEWARE = [
@@ -30,6 +35,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'manage_heros.urls'
