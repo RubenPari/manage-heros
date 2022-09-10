de521fa6aec223978c99fdb3f67dd04138fa00d6
RubenPari <rubenpari.developer@gmail.com>
2022-09-10 19:48:48 +0200

    added Comic class

--- comics/models.py
+++ comics/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Comic(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    isbn = models.CharField(max_length=13)
+    ean = models.CharField(max_length=13)
+    format = models.CharField(max_length=255)
+    pageCount = models.IntegerField()
+    resourceURI = models.TextField()
+    # array of urls
+    urls = models.TextField()
+    thumbnail = models.TextField()
+
+    def __str__(self):
+        return self.title
